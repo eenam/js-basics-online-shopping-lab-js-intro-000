86333f842753856e1b66926546e3f86ebe76251f
Emma Nam <en2458@gmail.com>
2020-11-13 20:42:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ function viewCart() {
   }
 
   var startSentence = "In your cart, you have"
-
-    if (cart.length === 1) {
-    return `In your cart, you have ${itemName} at ${itemPrice}.`
-  } else if (cart.length === 2) {
-    return `In your cart, you have ${itemName} at ${itemPrice}, and ${itemName} at ${itemPrice}.`
-  } else {
-    for (let i = 0; i < cart.length; i++) {
+  for (let i = 0; i < cart.length; i++) {
     return `In your cart, you have ${itemName} at ${itemPrice}, ${itemName} at ${itemPrice}, and ${itemName} at ${itemPrice}.`
     }
+  if (cart.length === 1) {
+    return `In your cart, you have ${itemName} at ${itemPrice}.`
+} else if (cart.length === 2) {
+    return `In your cart, you have ${itemName} at ${itemPrice}, and ${itemName} at ${itemPrice}.`
+} else {
+
 }
 }
 
