382531564f1eb089175ca0153992e5711d68ce76
Emma Nam <en2458@gmail.com>
2020-11-13 19:37:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,28 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  var itemObj = {itemName: itemName, itemPrice: Math.floor(Math.random() * 100) + 1}
+  cart.push(itemObj);
+  return (`${itemName} has been added to your cart.`)
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${itemName} at ${itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${itemName} at ${itemPrice}, and ${itemName} at ${itemPrice}.`
+  } else {
+    return `In your cart, you have ${itemName} at ${itemPrice}, ${itemName} at ${itemPrice}, and ${itemName} at ${itemPrice}.`
+    for (let i = 0; i < cart.length, i++)
 }
+}
+
+
+
+
 
 function total() {
   // write your code here
