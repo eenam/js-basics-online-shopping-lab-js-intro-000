3c420285384f59cbf7f2af1d209a1f9a44dd9a75
Emma Nam <en2458@gmail.com>
2020-11-13 20:13:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function addToCart(itemName) {
 function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
-  } else if (cart.length === 1) {
+  } 
+  
+
+  else if (cart.length === 1) {
     return `In your cart, you have ${itemName} at ${itemPrice}.`
   } else if (cart.length === 2) {
     return `In your cart, you have ${itemName} at ${itemPrice}, and ${itemName} at ${itemPrice}.`
